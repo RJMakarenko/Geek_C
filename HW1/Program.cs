e58d6edf1f806e8f8141004b6494b59f3acc235f
Ruslan Makarenko <rj.makarenko@mail.ru>
2022-09-18 15:14:59 +0300

    Первое задание из ДЗ1

---
+++ HW1/Program.cs
@@ -0,0 +1,15 @@
+﻿// Task 1
+Console.Write("Введите число a: ");
+int a = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите число b: ");
+int b = Convert.ToInt32(Console.ReadLine());
+if (a > b)
+{
+    Console.Write("max = ");
+    Console.WriteLine(a);
+}
+else
+{
+    Console.Write("max = ");
+    Console.WriteLine(b);
+}
