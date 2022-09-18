6625c711786f967b4d2fcfebd65e202f68ddcbae
Ruslan Makarenko <rj.makarenko@mail.ru>
2022-09-18 15:53:26 +0300

    Задачи 3 и 4 из ДЗ1

---
+++ HW1_Task3/Program.cs
@@ -0,0 +1,11 @@
+﻿// Task 3
+Console.Write("Введите число: ");
+int a = Convert.ToInt32(Console.ReadLine());
+if (a % 2 == 0)
+{
+    Console.WriteLine("Да, число чётное");
+}
+else
+{
+    Console.WriteLine("Нет, число нечётное");
+}
---
+++ HW1_Task4/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.Write("Введите число: ");
+int N = Convert.ToInt32(Console.ReadLine());
+int i = 2;
+while (i <= N)
+{
+    if (i % 2 ==0)
+    {
+        Console.Write(i);
+        Console.Write(" ");
+    }
+    i++;
+}
