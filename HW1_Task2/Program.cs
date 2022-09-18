d718327c7cb1575d20bdc8aaa77d3f964921a9d9
Ruslan Makarenko <rj.makarenko@mail.ru>
2022-09-18 15:31:31 +0300

    Второе задание ДЗ1

---
+++ HW1_Task2/Program.cs
@@ -0,0 +1,18 @@
+﻿// Task 2
+Console.Write("Введите число a: ");
+int a = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите число b: ");
+int b = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите число c: ");
+int c = Convert.ToInt32(Console.ReadLine());
+int max = a;
+if (b > max) 
+{
+    max = b;
+}
+if (c > max)    
+{
+    max = c;
+}
+Console.Write("Максимальное: ");
+Console.WriteLine(max);
